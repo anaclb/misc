6f1401332072a03cca15202db3dfd3850edc04e2
anaclb <anacb@pm.me>
2021-10-20 13:00:42 +0100

    file upload
    my solutions to hackerrank challenges

---
+++ hackerrank.py
@@ -0,0 +1,19 @@
+import math
+
+def plusMinus(arr):
+    print("{:.6f}".format(len(list(filter(lambda x : x > 0, arr)))/n))
+    print("{:.6f}".format(len(list(filter(lambda x : x < 0, arr)))/n))
+    print("{:.6f}".format(len(list(filter(lambda x : x == 0, arr)))/n))
+
+def miniMaxSum(arr):
+    arr.sort()
+    print(sum(arr[:-1]), sum(arr[1:]))
+
+def timeConversion(s):
+    time = s.split(":")
+    if time[-1].find("AM") != -1:
+        if time[0] == "12":
+            time[0] = "00"
+    elif time[0] != "12":
+        time[0] = str(int(time[0])+12)
+    return ':'.join(time)[:-2]
